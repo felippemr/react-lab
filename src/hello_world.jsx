c890e759982806b8330d64cb194b866494c073f2
felippemr <raposo.felippe@gmail.com>
2016-08-18 00:53:12 -0300

    Improve hello world

--- src/hello_world.jsx
+++ src/hello_world.jsx
@@ -1,7 +1,15 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+class Hello extends React.Component {
+    render(){
+        return (
+            <h1>Hello World!</h1>
+        )
+    }
+}
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <Hello />,
   document.getElementById('example')
 );
