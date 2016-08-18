1e82941daa32dc2781a389053fd576b08787b6ed
felippemr <raposo.felippe@gmail.com>
2016-08-18 00:54:16 -0300

    Fix hello world indents

--- src/hello_world.jsx
+++ src/hello_world.jsx
@@ -2,11 +2,11 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 class Hello extends React.Component {
-    render(){
-        return (
-            <h1>Hello World!</h1>
-        )
-    }
+  render(){
+    return (
+      <h1>Hello World!</h1>
+    )
+  }
 }
 
 ReactDOM.render(
